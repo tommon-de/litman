8c8c8e558614eecf81a2755ed1f3099c613b8adb
tommon-de <18406605799@163.com>
2021-08-13 19:33:44 +0800

    set stauts vaild

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :name,:password,:email, length: { maximum: 255 }
+  validates :status, inclusion: { in: [0,1],message: "%{value} is not a valid stauts" }
 
 end
