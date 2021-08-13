2257d8acddec9745b603f35e5dd1aeb5ea9a9c57
tommon-de <18406605799@163.com>
2021-08-13 20:32:18 +0800

    create seed and set user litman

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   enum status: { active: 0, inactive: 1}
+  
   validates :name,:password,:email, length: { maximum: 255 }
   validates :status, inclusion: {in: %w(active inactive)}
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(email: "admin@relationshipapp.com", name: "litman", password:'test1234', status:0)
