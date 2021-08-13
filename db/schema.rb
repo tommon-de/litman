19f7314f3c7ff426ef1b7ca7e594d925d5b26acc
tommon-de <18406605799@163.com>
2021-08-13 20:55:53 +0800

    add article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_123716) do
+ActiveRecord::Schema.define(version: 2021_08_13_125439) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "user_id"
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
