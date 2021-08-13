d1a953f99a8f9f4ca9b53c4086950ac5c5e280be
tommon-de <18406605799@163.com>
2021-08-13 21:02:37 +0800

    add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   enum status: { active: 0, inactive: 1}
-  
+
   validates :name,:password,:email, length: { maximum: 255 }
   validates :status, inclusion: {in: %w(active inactive)}
+
+  has_many :articles
 end
