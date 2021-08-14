8b84567016738f6c7555856cc437e7ffb8e6a3ee
tommon-de <18406605799@163.com>
2021-08-14 16:29:31 +0800

    add fetch method

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,15 @@
 class Animal < ApplicationRecord
-  
+  include HTTParty
+
+    response = HTTParty.get("https://axoltlapi.herokuapp.com/",verify: false)
+    response =  JSON.parse(response.to_json)
+    animal = Animal.new(
+      url:response['url'],
+      facts:response['facts'],
+      pics_repo:response['pics_repo'],
+      api_repo:response['api_repo']
+      )
+
+
+
 end
